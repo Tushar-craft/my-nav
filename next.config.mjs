7db6b7385a905c6ce1cdce5194cf9191c9cde867
Tushar280 <tushar.tushar@craftisque.com>
2025-06-09 18:57:24 +0530

    For github pages

--- next.config.mjs
+++ next.config.mjs
@@ -3,4 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
+const isGithubPages = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  output: 'export',
+  basePath: isGithubPages ? '/my-nav' : '',
+  assetPrefix: isGithubPages ? '/my-nav/' : '',
+};
+
+
 export default nextConfig;
