4302d23504058ba54fa4bbfcdf412c6ac488ad61
Tushar280 <tushar.tushar@craftisque.com>
2025-06-14 18:00:43 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
+const isGithubPages = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: isGithubPages ? '/my-nav' : '',
+  assetPrefix: isGithubPages ? '/my-nav/' : '',
 };
 
 export default nextConfig;
